36ec4f383dd9da62770c7d6c0f9aa907c2ca826b
Ryan <ryan@dksit.com>
2019-03-21 07:43:23 -0600

    x and o solved with forEach

--- exesAndOhs.js
+++ exesAndOhs.js
@@ -1,4 +1,5 @@
-// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. 
+//The string can contain any char.
 
 // Examples input/output:
 
@@ -7,3 +8,26 @@
 // XO(ooxXm) => true
 // XO(zpzpzpp) => true // when no 'x' and 'o' is present should return true
 // XO(zzoo) => false
+
+let XO = (str) => {
+    let lower = str.toLowerCase().split('')
+    let x = 0
+    let o = 0
+    lower.forEach(val => {
+        if(val === "x"){
+            x += 1
+        }
+        if(val === "o"){
+            o += 1
+        }
+    })
+    return x === o
+
+}
+console.log(XO('ooxx'))
+console.log(XO('xooxx'))
+console.log(XO('ooxXm'))
+console.log(XO('zpzpzpp'))
+console.log(XO('zzoo)'))
+
+//check how you can use reduce
